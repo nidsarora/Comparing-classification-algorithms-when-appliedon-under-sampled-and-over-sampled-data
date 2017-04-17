1169d9136ba1bfe3bc2a458ab9b7a1e92f49ee33
nidsarora <aroranidhi10@gmail.com>
2017-04-17 10:46:45 -0400

    added comments

--- MainFile.py
+++ MainFile.py
@@ -1,3 +1,5 @@
+_author_="nidhi"
+
 import os
 import subprocess
 from sklearn.ensemble import RandomForestClassifier
@@ -13,6 +15,10 @@ from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn import metrics
 from  ADASYN import adasyn
 style.use('ggplot')
+
+"""
+   Code to start a spark session
+"""
 filepath= "C:\\Users\\arora\\Documents\\nidhi\\big data project"
 pyspark_submit_args = os.environ.get("PYSPARK_SUBMIT_ARGS", "")
 if not "pyspark-shell" in pyspark_submit_args: pyspark_submit_args += " pyspark-shell"
@@ -20,40 +26,58 @@ os.environ["PYSPARK_SUBMIT_ARGS"] = pyspark_submit_args
 sc =SparkContext()
 print(sc)
 
-
+"""
+   Creating test data and training data
+"""
 df1=pd.DataFrame.from_csv('cs-training-processed.csv',sep=',')
 df2=pd.DataFrame.from_csv('cs-training.csv',sep=',')
 df_15000=df1.sample(15000)
 df1_15000=df2.sample(15000)
-# print(df_500)
 msk = np.random.rand(len(df_15000))<0.8
 training_data_sampled =df_15000[msk]
 X_training_data =df_15000[msk]
 test_data_sampled= df_15000[~msk]
 X_test_data= df_15000[~msk]
 
+"""
+   Creating raw DataFrame with all data
+"""
 rawDataFrame=pd.DataFrame.from_csv('cs-training.csv',sep=',')
 print(rawDataFrame.shape)
 rawDataPlot=plt.plot(rawDataFrame['DebtRatio'],'.')
 plt.show(rawDataPlot)
+"""
+   Creating raw DataFrame with removed ouliers for DebtRatio
+"""
 removedOutlierDebt=rawDataFrame[~(rawDataFrame["DebtRatio"] > 1)]
 print(removedOutlierDebt.shape)
 removedDebtOutlierPlot=plt.plot(removedOutlierDebt['DebtRatio'],'.')
 plt.show(removedDebtOutlierPlot)
+"""
+   Creating  DataFrame with removed ouliers for 'NumberOfTime30-59DaysPastDueNotWorse'
+"""
 raw30DayPastPlot=plt.plot(removedOutlierDebt['NumberOfTime30-59DaysPastDueNotWorse'],'.')
 plt.show(raw30DayPastPlot)
 removedOutlier30to59DaysPast=removedOutlierDebt[~(removedOutlierDebt["NumberOfTime30-59DaysPastDueNotWorse"] > 8)]
 removedOutlier30to59DaysPastPlot=plt.plot(removedOutlier30to59DaysPast['NumberOfTime30-59DaysPastDueNotWorse'],'.')
 plt.show(removedOutlier30to59DaysPastPlot)
+"""
+   Creating  DataFrame with removed ouliers for "RevolvingUtilizationOfUnsecuredLines"
+"""
 removeOutlierUnsecuredUtilization=removedOutlier30to59DaysPast[~(removedOutlier30to59DaysPast["RevolvingUtilizationOfUnsecuredLines"] > 1)]
 removeOutlierUnsecuredUtilizationPlot=plt.plot(removeOutlierUnsecuredUtilization['RevolvingUtilizationOfUnsecuredLines'],'.')
 plt.show(removeOutlierUnsecuredUtilizationPlot)
 print(removeOutlierUnsecuredUtilization.shape)
+"""
+   Creating  DataFrame with removed ouliers for Monthly Income
+"""
 MonthlyIncomePlot=plt.plot(removeOutlierUnsecuredUtilization['MonthlyIncome'],'.')
 plt.show(MonthlyIncomePlot)
 removedOutlierMonthlyIncome=removeOutlierUnsecuredUtilization[~(removeOutlierUnsecuredUtilization["MonthlyIncome"] > 300000)]
 print(removedOutlierMonthlyIncome.shape)
-
+"""
+Removing the missing values
+"""
 df_final=removedOutlierMonthlyIncome
 df_after_missing_removal=df_final[~df_final.isnull().any(axis=1)]#missing values with'NA' are removed
 print(df_after_missing_removal.shape)
@@ -62,22 +86,31 @@ print(df_after_missing_removal.corr()['SeriousDlqin2yrs'])
 removedOutlierMonthlyIncomePlot=plt.plot(removedOutlierMonthlyIncome['MonthlyIncome'])
 plt.show(removedOutlierMonthlyIncomePlot)
 print('---------------------------------')
+
+"""
+   Undersampling data
+"""
 df_non_defaulter=df_after_missing_removal[~(removeOutlierUnsecuredUtilization["SeriousDlqin2yrs"]==1)]
 df_defauter=df_after_missing_removal[~(removeOutlierUnsecuredUtilization["SeriousDlqin2yrs"]==0)]
 df_non_defaulter_sample=df_non_defaulter.sample(6582)
 frames=[df_non_defaulter_sample,df_defauter]
 undersampled_data=pd.concat(frames)
 
+"""
+  Preparing data for applying classification algorithms
+"""
 Y_train=X_training_data.pop('SeriousDlqin2yrs')
 Y_test=X_test_data.pop('SeriousDlqin2yrs')
-# Y_test.columns=['SeriousDlqin2yrs']
+Y_test.columns=['SeriousDlqin2yrs']
 X_train = pd.get_dummies(training_data_sampled)
 X_test=pd.get_dummies(test_data_sampled)
 X_train.columns.difference(X_test.columns)
 X_test[X_train.columns.difference(X_test.columns)] = 0
 X_test = X_test[X_train.columns]
 
-
+"""
+   Applying Decision Tree Classification Algorithm with depth 2 and 10
+"""
 dtree = DecisionTreeClassifier(criterion='entropy', random_state=0)
 dtree.fit(X_train,Y_train)
 print("Error using depth 2 is ")
@@ -89,14 +122,17 @@ print("Error using depth 10 is ")
 print(metrics.mean_absolute_error(Y_test, dtree.predict(X_test)))
 print(metrics.mean_squared_error(Y_test, dtree.predict(X_test)))
 
-logistic_model=LogisticRegression(penalty='l2',C=1)
+"""logistic_model=LogisticRegression(penalty='l2',C=1)
 logistic_model.fit(X_train,Y_train)
 print(metrics.accuracy_score(Y_test,logistic_model.predict(X_test)))
-print(metrics.mean_absolute_error(Y_test,logistic_model.predict(X_test)))
+print(metrics.mean_absolute_error(Y_test,logistic_model.predict(X_test)))"""
 
+"""
+  Naive Bayes Classification algorithm
+"""
 def NaiveBayes(x):
- # df_n = df_after_missing_removal.sample(1102*x)
- df_n=undersampled_data.sample(131*x)
+ # df_n = df_after_missing_removal.sample(1102*x) //used when simple dataset is used
+ df_n=undersampled_data.sample(131*x) #used when algorithm is applied on undersampled data
  msk = np.random.rand(len(df_n)) < 0.8
  training_data_sampled = df_n[msk]
  X_training_data = df_n[msk]
@@ -121,11 +157,17 @@ def NaiveBayes(x):
  print(metrics.classification_report(Y_test,y_pred))
 
 # NaiveBayes(100)
+"""
+   Function to oversample data
+"""
 def oversample(X_train,Y_train): # ADASYN is a python module that implements an adaptive oversampling technique for skewed datasets.
  adsn = adasyn.ADASYN(k=7,imb_threshold=0.6, ratio=0.75)
  new_X_train, new_Y_train = adsn.fit_transform(X_train,Y_train)
  return new_X_train,new_Y_train
 
+"""
+   Function to compare various classification algorithms based on some parameters
+"""
 def error_formula_logistic(x):
  # df_n = df_after_missing_removal.sample(1102*x)
  df_n=undersampled_data.sample(131*x)
@@ -145,25 +187,18 @@ def error_formula_logistic(x):
  logistic_model = LogisticRegression(penalty='l2', C=1)
  result=logistic_model.fit(X_train, Y_train)
  print(logistic_model.predict(X_test))
+ """
+    Parameters taken are precision score,accuracy score, recall score,f1 score and support
+ """
  y=metrics.precision_score(Y_test, result.predict(X_test))
  z=metrics.accuracy_score(Y_test, result.predict(X_test))
  a=metrics.recall_score(Y_test, result.predict(X_test))
  print(metrics.classification_report(Y_test, result.predict(X_test)))
  return y,z,a
 
-def visualize_tree(tree, feature_names):
-    with open("dt.dot", 'w') as f:
-        export_graphviz(tree, out_file=f,
-                        feature_names=feature_names)
-
-    command = ["dot", "-Tpng", "dt.dot", "-o", "dt.png"]
-    try:
-        subprocess.check_call(command)
-    except:
-        exit("Could not run dot, ie graphviz, to "
-             "produce visualization")
-
-
+ """
+    Function to evaluate how performance of decision tree varies with % of data taken
+ """
 def error_formula_decisiontree(x):
  # df_n = df_after_missing_removal.sample(1102*x)
  df_n=undersampled_data.sample(131*x)
@@ -206,7 +241,9 @@ def error_formula_decisiontree(x):
  print(metrics.classification_report(Y_test, dtree1.predict(X_test)))
  # return y, z, a
 
-
+ """
+    Function to evaluate how performance of Random Forest varies with % of data taken
+ """
 def error_formula_randomforest(x):
  # df_n = df_after_missing_removal.sample(1102*x)
  df_n=undersampled_data.sample(131*x)
@@ -232,7 +269,9 @@ def error_formula_randomforest(x):
  print("Random Forest Clssifier with full depth :Error is")
  print(metrics.classification_report(Y_test, rf1.predict(X_test)))
 
-
+ """
+    Function to draw graph based on % of input data taken to evaluate
+ """
 def graph(error_formula, x_range):
   x = np.array(x_range)
   y_list=[]
